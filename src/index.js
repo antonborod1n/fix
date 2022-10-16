ac89a99aebd9ca8ce8f943b5e4b01931dea6411e
antonborod1n <antonborodin86@gmail.com>
2022-10-16 11:33:29 +0300

    added content, aside

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import "./scss/main.scss";
 
+const upBtn = document.querySelector('.arrow-up'); // кнопка 
 
+window.onscroll = () => {
+    if (window.scrollY > 350) { // когда опускается на 350px появляется кнопка НАВЕРХ
+        upBtn.classList.add('arrow-up--active'); // Добавляем класс
+    } else {
+        upBtn.classList.remove('arrow-up--active'); // Удаляем класс
+    }
+};
 
